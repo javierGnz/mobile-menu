d6e78600a3cbf365244cbdb94a477c1ac794116e
Javier <gonzaleztorojavier@gmail.com>
2020-06-08 22:48:49 -0400

    implement gsap

--- src/js/toggleMenu.js
+++ src/js/toggleMenu.js
@@ -1,7 +1,13 @@
-const menu = document.querySelector('.menu');
+import { gsap } from "gsap";
+
+const menu = document.querySelector(".menu");
+let toggle = gsap.to(".btn-toggle-menu", {rotation: 90, x: -200, duration: 0.4, paused: true});
+
+toggle.reverse();
 
 const handleToggleMenu = () => {
-  menu.classList.toggle('menu--open');
-}
+  menu.classList.toggle("menu--open");
+  toggle.reversed(!toggle.reversed());
+};
 
 export default handleToggleMenu;
