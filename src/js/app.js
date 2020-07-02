16cc7ed098063d1b05790c13bd3d108fb5637c4d
Javier <gonzaleztorojavier@gmail.com>
2020-07-02 15:54:08 -0400

    profile dropdown addended

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,11 @@ import handleToggleDetail from "./toggleDetails";
 const btnMenu = document.querySelector('.btn-toggle-menu');
 const btnSubMenu = Array.from(document.querySelectorAll('.menu__item-title'));
 const btnAdd = document.querySelector('.btn--plus');
+const btnProfile = document.querySelector('.navbar__profile');
 
 btnMenu.addEventListener('click', handleToggleMenu);
 btnAdd.addEventListener('click', handleToggleDetail);
+btnProfile.addEventListener('click', () => {
+  document.querySelector('.profile').classList.toggle('profile--active');
+})
 handleToggleSubMenu(btnSubMenu);
